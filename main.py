f7c7ca00ff446760cea96c6f37950c66bd4a9dd8
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-05-11 21:14:33 +0530

    Added Glitchy Color Switch

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from func import *
 
 WIDTH, HEIGHT = 900, 600
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-FPS = 24
+FPS = 30
 
 BLACK = (0,0,0)
 BLUE = (5, 23, 61)
 WHITE = (255, 255, 255)
+GREEN = (20, 150, 40)
 
 pygame.display.set_caption("VAlgorithm")
 clock = pygame.time.Clock()
@@ -17,18 +18,19 @@ bar_region = pygame.Rect(125, 100, WIDTH, HEIGHT)
 bar_region.width -= bar_region.left*2
 bar_region.height -= bar_region.top*1.75
 
-bar_length = 50
+bar_length = 10
 bar_range = (40, bar_region.height - bar_region.top)
 bar_data = rand_arr(bar_length, bar_range)
+bar_color = BLUE
 
 start_sorting = 0
-def draw_bars(arr, bar_region):
+def draw_bars(arr, bar_region, color):
     gap = (bar_region.width)//len(arr)
     i = 0
 
     for e in arr:
         bar = pygame.Rect(bar_region.left + gap*i, bar_region.top, gap/1.5, e)
-        pygame.draw.rect(WIN, BLUE, bar)        
+        pygame.draw.rect(WIN, color, bar)        
         i += 1
 
 def end():
@@ -50,7 +52,7 @@ while True:
                 end()
 
             if event.key == pygame.K_s: #Initiate Sorting
-                start_sorting = 1
+                stages = my_sort(bar_data, 'bubble')
 
             if event.key == pygame.K_r: #Generate new bars
                 start_sorting = 0
@@ -58,17 +60,18 @@ while True:
                 stages = []
 
                 bar_data = rand_arr(bar_length, bar_range)
+                bar_color = BLUE
 
     WIN.fill(BLACK)
-    draw_bars(bar_data, bar_region)
+    draw_bars(bar_data, bar_region, bar_color)
 
-    if start_sorting:
-        stages = my_sort(bar_data, 'bubble')
-
-        if stage_index < len(stages) - 1:
+    if stages:
+        if stage_index < len(stages)-1:
             stage_index += 1
-            pygame.time.delay(100)
-            
+
         bar_data = stages[stage_index]
+
+        if bar_data == stages[-1]:
+            bar_color = GREEN
 
     pygame.display.update()
