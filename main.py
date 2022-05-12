03bf2e71f2b545882ae9faad949010ea2ef1dac5
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-05-12 14:06:37 +0530

    Glitchy Frame Delay

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.font.init()
 
 WIDTH, HEIGHT = 900, 600
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-FPS = 60
+FPS = 360
 
 TITLE = pygame.font.SysFont('cooper black', 80)
 PARA = pygame.font.SysFont('courier new', 40)
@@ -22,10 +22,17 @@ bar_region = pygame.Rect(125, 125, WIDTH, HEIGHT)
 bar_region.width -= bar_region.left*2
 bar_region.height -= bar_region.top*1.75
 
-bar_length = 30
+bar_number = 30
 bar_range = (40, bar_region.height - bar_region.top)
-bar_data = rand_arr(bar_length, bar_range)
+bar_data = rand_arr(bar_number, bar_range)
 bar_color = BLUE
+frame_delay = 50
+
+if bar_number > 20 and bar_number < 50:
+    frame_delay = 10
+
+if bar_number > 50:
+    frame_delay = 0
 
 stages = []
 stage_index = 0
@@ -47,11 +54,10 @@ while True:
                 stages = my_sort(bar_data, 'bubble')
 
             if event.key == pygame.K_r: #Generate new bars
-                start_sorting = 0
                 stage_index = 0
                 stages = []
 
-                bar_data = rand_arr(bar_length, bar_range)
+                bar_data = rand_arr(bar_number, bar_range)
                 bar_color = BLUE
 
     WIN.fill(BLACK)
@@ -61,10 +67,12 @@ while True:
     draw_bars(bar_data, bar_region, bar_color, WIN)
 
     if stages:
+        bar_data = stages[stage_index]
+
         if stage_index < len(stages)-1:
             stage_index += 1
 
-        bar_data = stages[stage_index]
+        pygame.time.delay(frame_delay)
 
         if bar_data == stages[-1]:
             pygame.time.delay(100)
